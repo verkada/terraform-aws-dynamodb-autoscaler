c4e86fd0cab5644959f273d835bb57ff258c1233
Siddarth Prakash <1428486+sidprak@users.noreply.github.com>
2020-04-30 21:29:49 -0400

    Add support for specifying GSI autoscaling params (#1)

--- main.tf
+++ main.tf
@@ -89,10 +89,10 @@ resource "aws_appautoscaling_target" "read_target" {
 }
 
 resource "aws_appautoscaling_target" "read_target_index" {
-  count              = var.enabled ? length(var.dynamodb_indexes) : 0
-  max_capacity       = var.autoscale_max_read_capacity
-  min_capacity       = var.autoscale_min_read_capacity
-  resource_id        = "table/${var.dynamodb_table_name}/index/${element(var.dynamodb_indexes, count.index)}"
+  for_each           = var.enabled ? var.dynamodb_indexes : {}
+  max_capacity       = lookup(each.value, "max_read_capacity", var.autoscale_max_read_capacity)
+  min_capacity       = lookup(each.value, "min_read_capacity", var.autoscale_min_read_capacity)
+  resource_id        = "table/${var.dynamodb_table_name}/index/${each.key}"
   scalable_dimension = "dynamodb:index:ReadCapacityUnits"
   service_namespace  = "dynamodb"
 }
@@ -116,17 +116,16 @@ resource "aws_appautoscaling_policy" "read_policy" {
 }
 
 resource "aws_appautoscaling_policy" "read_policy_index" {
-  count = var.enabled ? length(var.dynamodb_indexes) : 0
+  for_each = var.enabled ? var.dynamodb_indexes : {}
 
-  name = "DynamoDBReadCapacityUtilization:${element(
-    aws_appautoscaling_target.read_target_index.*.resource_id,
-    count.index
-  )}"
+  name = "DynamoDBReadCapacityUtilization:${
+    aws_appautoscaling_target.read_target_index[each.key].resource_id
+  }"
 
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.read_target_index.*.resource_id[count.index]
-  scalable_dimension = aws_appautoscaling_target.read_target_index.*.scalable_dimension[count.index]
-  service_namespace  = aws_appautoscaling_target.read_target_index.*.service_namespace[count.index]
+  resource_id        = aws_appautoscaling_target.read_target_index[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.read_target_index[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.read_target_index[each.key].service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
@@ -147,10 +146,10 @@ resource "aws_appautoscaling_target" "write_target" {
 }
 
 resource "aws_appautoscaling_target" "write_target_index" {
-  count              = var.enabled ? length(var.dynamodb_indexes) : 0
-  max_capacity       = var.autoscale_max_write_capacity
-  min_capacity       = var.autoscale_min_write_capacity
-  resource_id        = "table/${var.dynamodb_table_name}/index/${element(var.dynamodb_indexes, count.index)}"
+  for_each           = var.enabled ? var.dynamodb_indexes : {}
+  max_capacity       = lookup(each.value, "max_write_capacity", var.autoscale_max_write_capacity)
+  min_capacity       = lookup(each.value, "min_write_capacity", var.autoscale_min_write_capacity)
+  resource_id        = "table/${var.dynamodb_table_name}/index/${each.key}"
   scalable_dimension = "dynamodb:index:WriteCapacityUnits"
   service_namespace  = "dynamodb"
 }
@@ -174,17 +173,16 @@ resource "aws_appautoscaling_policy" "write_policy" {
 }
 
 resource "aws_appautoscaling_policy" "write_policy_index" {
-  count = var.enabled ? length(var.dynamodb_indexes) : 0
+  for_each = var.enabled ? var.dynamodb_indexes : {}
 
-  name = "DynamoDBWriteCapacityUtilization:${element(
-    aws_appautoscaling_target.write_target_index.*.resource_id,
-    count.index
-  )}"
+  name = "DynamoDBWriteCapacityUtilization:${
+    aws_appautoscaling_target.write_target_index[each.key].resource_id
+  }"
 
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.write_target_index.*.resource_id[count.index]
-  scalable_dimension = aws_appautoscaling_target.write_target_index.*.scalable_dimension[count.index]
-  service_namespace  = aws_appautoscaling_target.write_target_index.*.service_namespace[count.index]
+  resource_id        = aws_appautoscaling_target.write_target_index[each.key].resource_id
+  scalable_dimension = aws_appautoscaling_target.write_target_index[each.key].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.write_target_index[each.key].service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
